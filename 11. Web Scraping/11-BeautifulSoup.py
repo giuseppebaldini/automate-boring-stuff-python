d86d9bcc63bd75cacb83a3c633f1baf51fe46ed8
GiuseppeBaldini <40420789+GiuseppeBaldini@users.noreply.github.com>
2018-10-26 18:13:28 +0200

    Initial commit

--- 11. Web Scraping/11-BeautifulSoup.py
+++ 11. Web Scraping/11-BeautifulSoup.py
@@ -13,3 +13,38 @@ type(noStarchSoup)
 exampleFile = open('example.html')
 exampleSoup = bs4.BeautifulSoup(exampleFile)
 type(exampleSoup)
+
+# Web page elements can be retrieved using calling the select() method
+# and passing a string of the CSS selector for the element we are looking for
+
+# Examples of CSS Selectors
+
+Selector passed to the  select() method
+soup.select('div')
+will match...
+All elements named  <div>
+
+soup.select('#author')
+The element with an  id attribute of
+author
+
+soup.select('.notice')
+All elements that use a CSS  class
+attribute named  notice
+
+soup.select('div span')
+All elements named  <span> that are
+within an element named  <div>
+
+soup.select('div > span')
+All elements named  <span> that are
+directly within an element named  <div> ,
+with no other element in between
+
+soup.select('input[name]')
+All elements named  <input> that have a
+name attribute with any value
+
+soup.select('input[type="button"]')
+All elements named  <input> that have an
+attribute named  type with value  button
---
+++ 11. Web Scraping/11-Example.py
@@ -0,0 +1,33 @@
+# Parsing using BeautifulSoup
+
+# Example 1: elements with id="author"
+
+import bs4
+exampleFile = open('example.html')
+exampleSoup = bs4.BeautifulSoup(exampleFile.read())
+elems = exampleSoup.select('#author')
+type(elems)
+len(elems)
+type(elems[0])
+elems[0].getText()
+str(elems[0])
+elems[0].attrs
+
+# Example 2: all the  <p> elements
+
+pElems = exampleSoup.select('p')
+str(pElems[0])
+pElems[0].getText()
+str(pElems[1])
+pElems[1].getText()
+str(pElems[2])
+pElems[2].getText()
+
+# Example 3: getting data from an elem's attributes
+
+soup = bs4.BeautifulSoup(open('example.html'))
+spanElem = soup.select('span')[0]
+str(spanElem)
+spanElem.get('id')
+spanElem.get('some_nonexistent_addr') == None
+spanElem.attrs
