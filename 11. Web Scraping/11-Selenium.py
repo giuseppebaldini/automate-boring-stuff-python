fe02fd5af1cfe87b4382404f9bde9c2b8da22bf0
GiuseppeBaldini <40420789+GiuseppeBaldini@users.noreply.github.com>
2018-10-29 16:41:46 +0100

    Initial commit

--- 11. Web Scraping/11-Selenium.py
+++ 11. Web Scraping/11-Selenium.py
@@ -53,3 +53,31 @@ otherwise False
 
 location
 A dictionary with keys  'x' and  'y' for the position of the element in the page
+
+# Using the click() method to simulate a mouse
+# First we find the element, then we "click" it
+
+linkElem = browser.find_element_by_link_text('Example text')
+type(linkElem)
+linkElem.click()
+
+# Filling out and submitting forms
+# Usually it involves finding the  <input> or  <textarea> elements
+# and calling the send_keys() methods
+
+# Example with email
+
+emailELem = browser.find_element_by_id('login-username')
+emailELem.send_keys('just_an_email@email.com')
+passwordElem = browser.find_elements_by_id('login-passwd')
+passwordElem.send_keys('BesT_PasSwORd')
+passwordElem.submit()
+
+# Selenium can also simulate other keyboard keys which are not strings
+# e.g. arrow keys using the selenium.webdriver.common.keys module
+
+# Common browser buttons
+browser.back()
+browser.forward()
+browser.refresh()
+browser.quit()
---
+++ 12. Working with Excel/12.py
@@ -0,0 +1,1 @@
+# Chapter 12 - Working with Excel with openpyxl
